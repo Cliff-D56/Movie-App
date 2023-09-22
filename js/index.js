e39cc2dc428d493ffd035ef8df997d8a792164c5
Clifford Gardner <cliffordgardner9@gmail.com>
2023-09-22 16:53:55 -0500

    Bad update

--- js/index.js
+++ js/index.js
@@ -10,31 +10,31 @@ import {
 
 
 
-
-let ddlYears = document.getElementById("ddlYears");
-let currentYear = (new Date()).getFullYear();
-
-for (let i = 1950; i <= currentYear; i++) {
-    let option = document.createElement("OPTION");
-    option.innerHTML = i;
-    option.value = i;
-    ddlYears.appendChild(option);
-}
-
-let submit = document.querySelector('#add-btn');
-submit.addEventListener('click', add)
-
-    function add (e) {
-        e.preventDefault();
-        let title = document.querySelector("#title").value
-        let summary = document.querySelector('#summary').value
-        let year = document.querySelector("#ddlYears").value
-        let rating = document.querySelector("#rating").value
-        console.log(title);
-        console.log(summary);
-        console.log(year);
-        console.log(rating);
-    }
+//
+// let ddlYears = document.getElementById("ddlYears");
+// let currentYear = (new Date()).getFullYear();
+//
+// for (let i = 1950; i <= currentYear; i++) {
+//     let option = document.createElement("OPTION");
+//     option.innerHTML = i;
+//     option.value = i;
+//     ddlYears.appendChild(option);
+// }
+//
+// let submit = document.querySelector('#add-btn');
+// submit.addEventListener('click', add)
+//
+//     function add (e) {
+//         e.preventDefault();
+//         let title = document.querySelector("#title").value
+//         let summary = document.querySelector('#summary').value
+//         let year = document.querySelector("#ddlYears").value
+//         let rating = document.querySelector("#rating").value
+//         console.log(title);
+//         console.log(summary);
+//         console.log(year);
+//         console.log(rating);
+//     }
 
 
 
